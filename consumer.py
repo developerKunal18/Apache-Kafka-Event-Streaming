6f12b5d4c8b4e5c33dda2d56fa85c08613d2860f
kunal karbhari <fullstackdeveloperkunal18@gmail.com>
2026-07-21 22:52:27 +0530

    Create consumer.py

---
+++ consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "orders",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+)
+
+print("Waiting for events...")
+
+for message in consumer:
+
+    print(message.value)
